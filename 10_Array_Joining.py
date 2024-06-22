90e345b4e022b68bb9e3619944afa63f862ae6d5
UmarAjmal <126502013+UmarAjmal@users.noreply.github.com>
2024-06-22 11:53:53 +0500

    Update

--- 10_Array_Joining.py
+++ 10_Array_Joining.py
@@ -20,3 +20,30 @@ arr2 = np.array([[7, 8, 9],[10, 11, 12]])
 arr = np.stack((arr1, arr2), axis=1)
 print(arr)
 
+# Stacking along with row. using hstack()
+import numpy as np
+
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+
+arr = np.hstack((arr1, arr2))
+print(arr)
+
+#  Stacking along with column. using vstack()
+import numpy as np
+
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+
+arr = np.vstack((arr1, arr2))
+print(arr)
+
+##  Stacking along with height(depth) . using dstack()
+import numpy as np
+
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+
+arr = np.dstack((arr1, arr2))
+print(arr)
+
